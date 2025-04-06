af077d2e81a59602041cef4d951ea2329df71709
Hien Nguyen <hien.nguyen2@myy.haaga-helia.fi>
2025-04-06 16:27:37 +0300

    fix for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import Statistics from "./component/Statistics.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Navigate to="/home" replace />} />
